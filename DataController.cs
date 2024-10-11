40eadc68d5fb1893f1898d206d1d33566f7502bf
Shahid <shahidkhudadad@hotmail.com>
2024-10-11 01:56:29 +0100

    Changes in Controller

---
+++ DataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController:Controller
+    {
+        private IDataRepository _dataRepository;
+        public DataController(IDataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+        //api/data
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            var data = _dataRepository.GetAllData();
+            return OK(data);
+        }
+
+        //api/data
+        [HttpPost]
+        public IActionResult Createdata([FromBody] Data dataToCreate)
+        {
+            if (dataToCreate == null)
+                return BadRequest(ModelState);
+            if (!_dataRepository.CreateData(dataToCreate))
+                return StatusCode(500);
+
+        }
+    }
+
+    
+}
